dc5a86a620b7ccc3704acb04a61745c1b62844de
pvsivap <grt.sivaprasad@gmail.com>
2016-03-16 20:22:47 +0530

    Added caption to lable

--- CustZero-Tours/CustZero-Tours/Form1.Designer.cs
+++ CustZero-Tours/CustZero-Tours/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(34, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(29, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "ADS";
             // 
             // Form1
             // 
