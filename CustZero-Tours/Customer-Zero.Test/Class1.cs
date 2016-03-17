ac068578d88cf375ff2a2f9efbaa5c3dd9e3fd5c
pvsivap <grt.sivaprasad@gmail.com>
2016-03-17 16:45:08 +0530

    Included two unit test cases.

--- CustZero-Tours/Customer-Zero.Test/Class1.cs
+++ CustZero-Tours/Customer-Zero.Test/Class1.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Customer_Zero.Test
 {
+    [TestFixture]
     public class Class1
     {
+        [TestCase]
+        public void VerifyDomasticImages()
+        {
+            //check this part. rrrrrr
+            bool isFileExist = true;
+            if (File.Exists("D:\\Siva\\Tours\\DelhiTour.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Kashmir.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Rshikesh.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Simla.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Agra-Mathura.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Amarnath.png"))
+                isFileExist = false;
+
+            Assert.AreEqual(true, isFileExist);
+        }
+
+        [TestCase]
+        public void VerifyInternationalImages()
+        {
+            bool isFileExist = true;
+            if (File.Exists("D:\\Siva\\Tours\\Africa.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\America.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Australia.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\China.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Greese.png"))
+                isFileExist = false;
+
+            if (File.Exists("D:\\Siva\\Tours\\Swiss.png"))
+                isFileExist = false;
+
+            Assert.AreEqual(true, isFileExist);
+        }
     }
 }
